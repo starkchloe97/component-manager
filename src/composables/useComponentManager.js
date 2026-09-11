1dfc344be665dd136f69f24dff037f43e444c727
starkchloe97 <starkchloe97@gmail.com>
2026-09-11 06:20:20 +0500

    Add global component manager

---
+++ src/composables/useComponentManager.js
@@ -0,0 +1,54 @@
+import { reactive, ref, computed } from "vue";
+
+const components = reactive({});
+const selectedComponentId = ref(null);
+const drawerOpen = ref(false);
+
+export function useComponentManager() {
+  const registerComponent = (id, config = {}) => {
+    if (!components[id]) {
+      components[id] = {
+        id,
+        ...config,
+      };
+    } else {
+      Object.assign(components[id], config);
+    }
+
+    if (!selectedComponentId.value) {
+      selectedComponentId.value = id;
+    }
+  };
+
+  const selectComponent = (id) => {
+    if (!components[id]) return;
+
+    selectedComponentId.value = id;
+  };
+
+  const openDrawer = (id = selectedComponentId.value) => {
+    if (id) selectComponent(id);
+    drawerOpen.value = true;
+  };
+
+  const closeDrawer = () => {
+    drawerOpen.value = false;
+  };
+
+  const selectedComponent = computed(() => {
+    if (!selectedComponentId.value) return null;
+
+    return components[selectedComponentId.value] || null;
+  });
+
+  return {
+    components,
+    selectedComponentId,
+    selectedComponent,
+    drawerOpen,
+    registerComponent,
+    selectComponent,
+    openDrawer,
+    closeDrawer,
+  };
+}
