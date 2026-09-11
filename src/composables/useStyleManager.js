b5a3b7d14207b006090e3f40b1963624215c8479
starkchloe97 <starkchloe97@gmail.com>
2026-09-11 06:20:46 +0500

    Centralize global component selection and drawer state

--- src/composables/useStyleManager.js
+++ src/composables/useStyleManager.js
@@ -1,14 +1,16 @@
-import { reactive, ref, computed } from "vue";
+import { computed, reactive, ref } from "vue";
 
 const components = reactive({});
 const selectedComponentId = ref(null);
+const drawerOpen = ref(false);
 
 export function useStyleManager() {
-  const registerComponent = (id, config) => {
-    components[id] = {
-      id,
-      ...config
-    };
+  const registerComponent = (id, config = {}) => {
+    if (!components[id]) {
+      components[id] = { id, ...config };
+    } else {
+      Object.assign(components[id], config);
+    }
 
     if (!selectedComponentId.value) {
       selectedComponentId.value = id;
@@ -16,24 +18,31 @@ export function useStyleManager() {
   };
 
   const selectComponent = (id) => {
-    if (components[id]) {
-      selectedComponentId.value = id;
-    }
+    if (components[id]) selectedComponentId.value = id;
+  };
+
+  const openDrawer = (id = selectedComponentId.value) => {
+    if (id) selectComponent(id);
+    drawerOpen.value = true;
+  };
+
+  const closeDrawer = () => {
+    drawerOpen.value = false;
   };
 
   const selectedComponent = computed(() => {
-    if (!selectedComponentId.value) {
-      return null;
-    }
-
-    return components[selectedComponentId.value];
+    if (!selectedComponentId.value) return null;
+    return components[selectedComponentId.value] || null;
   });
 
   return {
     components,
     selectedComponentId,
     selectedComponent,
+    drawerOpen,
     registerComponent,
-    selectComponent
+    selectComponent,
+    openDrawer,
+    closeDrawer,
   };
 }
