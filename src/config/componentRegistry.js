97208a764ffc5d54b111139f06571d7adf6aad38
starkchloe97 <starkchloe97@gmail.com>
2026-09-15 01:39:17 +0500

    sad

--- src/config/componentRegistry.js
+++ src/config/componentRegistry.js
@@ -2,46 +2,46 @@ export const componentRegistry = {
   TextImage1: {
     id: "TextImage1",
     name: "Text Image 1",
-    source: () => import("@/components/TextImage1.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage1.vue?raw"),
   },
   TextImage2: {
     id: "TextImage2",
     name: "Text Image 2",
-    source: () => import("@/components/TextImage2.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage2.vue?raw"),
   },
   TextImage3: {
     id: "TextImage3",
     name: "Text Image 3",
-    source: () => import("@/components/TextImage3.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage3.vue?raw"),
   },
   TextImage4: {
     id: "TextImage4",
     name: "Text Image 4",
-    source: () => import("@/components/TextImage4.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage4.vue?raw"),
   },
   TextImage5: {
     id: "TextImage5",
     name: "Text Image 5",
-    source: () => import("@/components/TextImage5.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage5.vue?raw"),
   },
   TextImage6: {
     id: "TextImage6",
     name: "Text Image 6",
-    source: () => import("@/components/TextImage6.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage6.vue?raw"),
   },
   TextImage7: {
     id: "TextImage7",
     name: "Text Image 7",
-    source: () => import("@/components/TextImage7.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage7.vue?raw"),
   },
   TextImage8: {
     id: "TextImage8",
     name: "Text Image 8",
-    source: () => import("@/components/TextImage8.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage8.vue?raw"),
   },
   TextImage9: {
     id: "TextImage9",
     name: "Text Image 9",
-    source: () => import("@/components/TextImage9.vue?raw"),
+    source: () => import("@/components/TextImage/TextImage9.vue?raw"),
   },
 };
