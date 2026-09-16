a02ce791b3ef2aaf0b5c2ad2a7675cb9a72d95ad
starkchloe97 <starkchloe97@gmail.com>
2026-09-17 00:28:08 +0500

    Use shared editor for testimonial components

--- src/pages/TestimonialView.vue
+++ src/pages/TestimonialView.vue
@@ -1,19 +1,11 @@
 <script setup>
-
-    import Testimonial1 from '@/components/Testimonials/Testimonial1.vue';
-    import Testimonial2 from '@/components/Testimonials/Testimonial2.vue';
-    import Testimonial3 from '@/components/Testimonials/Testimonial3.vue';
-    import Testimonial4 from '@/components/Testimonials/Testimonial4.vue';
-    import Testimonial5 from '@/components/Testimonials/Testimonial5.vue';
-
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
 <template>
-
-    <Testimonial1 />    
-    <Testimonial2 />
-    <Testimonial3 />
-    <Testimonial4 />
-    <Testimonial5 />
-
+  <ComponentLibraryPage
+    title="Testimonial Components"
+    prefix="Testimonial"
+    description="Browse every testimonial component and open the shared editor to customize any element inside it."
+  />
 </template>
