6a32eabf28ca2b08e8d306831581e336382ed488
starkchloe97 <starkchloe97@gmail.com>
2026-09-17 00:28:04 +0500

    Use shared editor for FAQ components

--- src/pages/FAQ.vue
+++ src/pages/FAQ.vue
@@ -1,20 +1,11 @@
 <script setup>
-
-import Faq1 from '@/components/FaqSection/Faq1.vue';
-import Faq2 from '@/components/FaqSection/Faq2.vue';
-import Faq3 from '@/components/FaqSection/Faq3.vue';
-import Faq4 from '@/components/FaqSection/Faq4.vue';
-import Faq5 from '@/components/FaqSection/Faq5.vue';
-
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
 <template>
-
-<Faq1 />
-<Faq2 />
-<Faq3 />
-<Faq4 />
-<Faq5 />
-
-
+  <ComponentLibraryPage
+    title="FAQ Components"
+    prefix="Faq"
+    description="Browse every FAQ component and open the shared editor to customize any element inside it."
+  />
 </template>
