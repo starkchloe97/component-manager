b75868e71015e404137227f99d8eedb8b06aa748
starkchloe97 <starkchloe97@gmail.com>
2026-09-17 00:28:00 +0500

    Use shared editor for CTA components

--- src/pages/CTA.vue
+++ src/pages/CTA.vue
@@ -1,32 +1,11 @@
 <script setup>
-
-import CtaSection1 from '@/components/CTASection/CtaSection1.vue';
-import CtaSection2 from '@/components/CTASection/CTASection2.vue';
-import CtaSection3 from '@/components/CTASection/CtaSection3.vue';
-import CtaSection4 from '@/components/CTASection/CtaSection4.vue';
-import CtaSection5 from '@/components/CTASection/CtaSection5.vue';
-import CtaSection6 from '@/components/CTASection/CtaSection6.vue';
-import CtaSection7 from '@/components/CTASection/CtaSection7.vue';
-import CtaSection8 from '@/components/CTASection/CtaSection8.vue';
-
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
 <template>
-    
-<CtaSection1/>
-
-<CtaSection8/>
-    
-<CtaSection2/>
-
-<CtaSection3/>
-
-<CtaSection4/>
-
-<CtaSection5/>
-
-<CtaSection6/>
-
-<CtaSection7/>
-
+  <ComponentLibraryPage
+    title="CTA Components"
+    prefix="CtaSection"
+    description="Browse every CTA component and open the shared editor to customize any element inside it."
+  />
 </template>
