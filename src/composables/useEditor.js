c954df821f7b54395467687f3a3550e2a19b33d9
starkchloe97 <starkchloe97@gmail.com>
2026-09-23 05:39:13 +0500

    Expose standalone section insertion from useEditor

--- src/composables/useEditor.js
+++ src/composables/useEditor.js
@@ -341,6 +341,7 @@ export function useEditor() {
     selectNode,
     addSection,
     addSectionAfter,
+    addStandaloneSectionAfter,
     addSectionToNode,
     addSectionToParent,
     addContainer,
