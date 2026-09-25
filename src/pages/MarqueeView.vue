6142c4687fc8b1aadf87398d705235f7a311c863
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 01:59:47 +0500

    Use shared editable library for Marquee Components

--- src/pages/MarqueeView.vue
+++ src/pages/MarqueeView.vue
@@ -1,34 +1,11 @@
 <script setup>
-
-    import Marquee1 from '@/components/MarqueeSection/Marquee1.vue';
-    import Marquee2 from '@/components/MarqueeSection/Marquee2.vue';
-    import Marquee3 from '@/components/MarqueeSection/Marquee3.vue';
-    import Marquee4 from '@/components/MarqueeSection/Marquee4.vue';
-    import Marquee5 from '@/components/MarqueeSection/Marquee5.vue';
-
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
 <template>
-
-    <Marquee1 />
-
-    <br>
-
-    <Marquee2 />
-
-    <br>
-
-    <Marquee3 />
-
-    <br>
-
-    <Marquee4 />
-
-    <br>
-
-    <Marquee5 />
-
-    
-
-
+  <ComponentLibraryPage
+    title="Marquee Components"
+    prefix="Marquee"
+    description="Browse every Marquee component and open the shared editor to customize any element inside it."
+  />
 </template>
