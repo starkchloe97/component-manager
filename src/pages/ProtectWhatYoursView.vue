7aa085f2a4dfedb71c243e6a077e0b06ecb51fa5
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 01:59:52 +0500

    Use shared editable library for Protect What Yours Components

--- src/pages/ProtectWhatYoursView.vue
+++ src/pages/ProtectWhatYoursView.vue
@@ -1,21 +1,11 @@
-MarqueeView<script setup>
-
-    import ProtectWhatYours1 from '@/components/ProtectWhatYours/ProtectWhatYours1.vue';
-    import ProtectWhatYours2 from '@/components/ProtectWhatYours/ProtectWhatYours2.vue';
-    import ProtectWhatyours3 from '@/components/ProtectWhatYours/ProtectWhatyours3.vue';
-    import ProtectWhatYours4 from '@/components/ProtectWhatYours/ProtectWhatYours4.vue';
-    import ProtectWhatYours5 from '@/components/ProtectWhatYours/ProtectWhatYours5.vue';
-
-
+<script setup>
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
-
 <template>
-
-    <ProtectWhatYours1 />
-    <ProtectWhatYours2 />
-    <ProtectWhatyours3 />
-    <ProtectWhatYours4 />
-    <ProtectWhatYours5 />
-
+  <ComponentLibraryPage
+    title="Protect What Yours Components"
+    prefix="ProtectWhatYours"
+    description="Browse every Protect What Yours component and open the shared editor to customize any element inside it."
+  />
 </template>
