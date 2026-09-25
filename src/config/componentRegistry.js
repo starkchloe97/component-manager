64ca113070159586c621597c2c2197a3ab69aacc
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 01:59:19 +0500

    Register all newly added components for shared editing

--- src/config/componentRegistry.js
+++ src/config/componentRegistry.js
@@ -95,9 +95,143 @@ import LLC3Source from "@/components/LLCPackages/LLC3.vue?raw";
 import LLC4Source from "@/components/LLCPackages/LLC4.vue?raw";
 
 
+import Box1 from "@/components/BoxesSection/Box1.vue";
+import Box1Source from "@/components/BoxesSection/Box1.vue?raw";
+import Box2 from "@/components/BoxesSection/Box2.vue";
+import Box2Source from "@/components/BoxesSection/Box2.vue?raw";
+import Box3 from "@/components/BoxesSection/Box3.vue";
+import Box3Source from "@/components/BoxesSection/Box3.vue?raw";
+import Box4 from "@/components/BoxesSection/Box4.vue";
+import Box4Source from "@/components/BoxesSection/Box4.vue?raw";
+import Box5 from "@/components/BoxesSection/Box5.vue";
+import Box5Source from "@/components/BoxesSection/Box5.vue?raw";
+import Content1 from "@/components/ContentSection/Content1.vue";
+import Content1Source from "@/components/ContentSection/Content1.vue?raw";
+import Content2 from "@/components/ContentSection/Content2.vue";
+import Content2Source from "@/components/ContentSection/Content2.vue?raw";
+import Content3 from "@/components/ContentSection/Content3.vue";
+import Content3Source from "@/components/ContentSection/Content3.vue?raw";
+import DoAndDont1 from "@/components/DoAndDontSection/DoAndDont1.vue";
+import DoAndDont1Source from "@/components/DoAndDontSection/DoAndDont1.vue?raw";
+import DoAndDont2 from "@/components/DoAndDontSection/DoAndDont2.vue";
+import DoAndDont2Source from "@/components/DoAndDontSection/DoAndDont2.vue?raw";
+import DoAndDont3 from "@/components/DoAndDontSection/DoAndDont3.vue";
+import DoAndDont3Source from "@/components/DoAndDontSection/DoAndDont3.vue?raw";
+import DoAndDont4 from "@/components/DoAndDontSection/DoAndDont4.vue";
+import DoAndDont4Source from "@/components/DoAndDontSection/DoAndDont4.vue?raw";
+import Feature1 from "@/components/FeaturesSection.vue/Feature1.vue";
+import Feature1Source from "@/components/FeaturesSection.vue/Feature1.vue?raw";
+import Feature2 from "@/components/FeaturesSection.vue/Feature2.vue";
+import Feature2Source from "@/components/FeaturesSection.vue/Feature2.vue?raw";
+import Feature3 from "@/components/FeaturesSection.vue/Feature3.vue";
+import Feature3Source from "@/components/FeaturesSection.vue/Feature3.vue?raw";
+import Feature4 from "@/components/FeaturesSection.vue/Feature4.vue";
+import Feature4Source from "@/components/FeaturesSection.vue/Feature4.vue?raw";
+import Feature5 from "@/components/FeaturesSection.vue/Feature5.vue";
+import Feature5Source from "@/components/FeaturesSection.vue/Feature5.vue?raw";
+import Feature6 from "@/components/FeaturesSection.vue/Feature6.vue";
+import Feature6Source from "@/components/FeaturesSection.vue/Feature6.vue?raw";
+import Feature7 from "@/components/FeaturesSection.vue/Feature7.vue";
+import Feature7Source from "@/components/FeaturesSection.vue/Feature7.vue?raw";
+import Feature8 from "@/components/FeaturesSection.vue/Feature8.vue";
+import Feature8Source from "@/components/FeaturesSection.vue/Feature8.vue?raw";
+import Feature9 from "@/components/FeaturesSection.vue/Feature9.vue";
+import Feature9Source from "@/components/FeaturesSection.vue/Feature9.vue?raw";
+import Feature10 from "@/components/FeaturesSection.vue/Feature10.vue";
+import Feature10Source from "@/components/FeaturesSection.vue/Feature10.vue?raw";
+import Feature11 from "@/components/FeaturesSection.vue/Feature11.vue";
+import Feature11Source from "@/components/FeaturesSection.vue/Feature11.vue?raw";
+import Feature12 from "@/components/FeaturesSection.vue/Feature12.vue";
+import Feature12Source from "@/components/FeaturesSection.vue/Feature12.vue?raw";
+import Feature13 from "@/components/FeaturesSection.vue/Feature13.vue";
+import Feature13Source from "@/components/FeaturesSection.vue/Feature13.vue?raw";
+import Marquee1 from "@/components/MarqueeSection/Marquee1.vue";
+import Marquee1Source from "@/components/MarqueeSection/Marquee1.vue?raw";
+import Marquee2 from "@/components/MarqueeSection/Marquee2.vue";
+import Marquee2Source from "@/components/MarqueeSection/Marquee2.vue?raw";
+import Marquee3 from "@/components/MarqueeSection/Marquee3.vue";
+import Marquee3Source from "@/components/MarqueeSection/Marquee3.vue?raw";
+import Marquee4 from "@/components/MarqueeSection/Marquee4.vue";
+import Marquee4Source from "@/components/MarqueeSection/Marquee4.vue?raw";
+import Marquee5 from "@/components/MarqueeSection/Marquee5.vue";
+import Marquee5Source from "@/components/MarqueeSection/Marquee5.vue?raw";
+import Process1 from "@/components/ProcessSection/Process1.vue";
+import Process1Source from "@/components/ProcessSection/Process1.vue?raw";
+import ProtectWhatYours1 from "@/components/ProtectWhatYours/ProtectWhatYours1.vue";
+import ProtectWhatYours1Source from "@/components/ProtectWhatYours/ProtectWhatYours1.vue?raw";
+import ProtectWhatYours2 from "@/components/ProtectWhatYours/ProtectWhatYours2.vue";
+import ProtectWhatYours2Source from "@/components/ProtectWhatYours/ProtectWhatYours2.vue?raw";
+import ProtectWhatyours3 from "@/components/ProtectWhatYours/ProtectWhatyours3.vue";
+import ProtectWhatyours3Source from "@/components/ProtectWhatYours/ProtectWhatyours3.vue?raw";
+import ProtectWhatYours4 from "@/components/ProtectWhatYours/ProtectWhatYours4.vue";
+import ProtectWhatYours4Source from "@/components/ProtectWhatYours/ProtectWhatYours4.vue?raw";
+import ProtectWhatYours5 from "@/components/ProtectWhatYours/ProtectWhatYours5.vue";
+import ProtectWhatYours5Source from "@/components/ProtectWhatYours/ProtectWhatYours5.vue?raw";
+import Tab1 from "@/components/TabsSection/Tab1.vue";
+import Tab1Source from "@/components/TabsSection/Tab1.vue?raw";
+import Tab2 from "@/components/TabsSection/Tab2.vue";
+import Tab2Source from "@/components/TabsSection/Tab2.vue?raw";
+import WhyFileWithUs from "@/components/WhyFileWithUs/WhyFileWithUs.vue";
+import WhyFileWithUsSource from "@/components/WhyFileWithUs/WhyFileWithUs.vue?raw";
+import ProtectYourWork from "@/components/WhyFileWithUs/ProtectYourWork.vue";
+import ProtectYourWorkSource from "@/components/WhyFileWithUs/ProtectYourWork.vue?raw";
+import WhatYouCanRegister from "@/components/WhyFileWithUs/WhatYouCanRegister.vue";
+import WhatYouCanRegisterSource from "@/components/WhyFileWithUs/WhatYouCanRegister.vue?raw";
+import BrandRegistration from "@/components/WhyRegisterYourBrand/BrandRegistration.vue";
+import BrandRegistrationSource from "@/components/WhyRegisterYourBrand/BrandRegistration.vue?raw";
+import WhyWorkWithUsSection from "@/components/WhyWorkWithUsSection.vue";
+import WhyWorkWithUsSectionSource from "@/components/WhyWorkWithUsSection.vue?raw";
+import WhyRegisterCopyRight from "@/components/FeaturesSection.vue/WhyRegisterCopyRight.vue";
+import WhyRegisterCopyRightSource from "@/components/FeaturesSection.vue/WhyRegisterCopyRight.vue?raw";
+
 const entry = (id, name, component, source) => ({ id, name, component, source });
 
 export const componentRegistry = {
+  Box1: entry("Box1", "Box1", Box1, Box1Source),
+  Box2: entry("Box2", "Box2", Box2, Box2Source),
+  Box3: entry("Box3", "Box3", Box3, Box3Source),
+  Box4: entry("Box4", "Box4", Box4, Box4Source),
+  Box5: entry("Box5", "Box5", Box5, Box5Source),
+  Content1: entry("Content1", "Content 1", Content1, Content1Source),
+  Content2: entry("Content2", "Content 2", Content2, Content2Source),
+  Content3: entry("Content3", "Content 3", Content3, Content3Source),
+  DoAndDont1: entry("DoAndDont1", "Do & Don't 1", DoAndDont1, DoAndDont1Source),
+  DoAndDont2: entry("DoAndDont2", "Do & Don't 2", DoAndDont2, DoAndDont2Source),
+  DoAndDont3: entry("DoAndDont3", "Do & Don't 3", DoAndDont3, DoAndDont3Source),
+  DoAndDont4: entry("DoAndDont4", "Do & Don't 4", DoAndDont4, DoAndDont4Source),
+  Feature1: entry("Feature1", "Feature 1", Feature1, Feature1Source),
+  Feature2: entry("Feature2", "Feature 2", Feature2, Feature2Source),
+  Feature3: entry("Feature3", "Feature 3", Feature3, Feature3Source),
+  Feature4: entry("Feature4", "Feature 4", Feature4, Feature4Source),
+  Feature5: entry("Feature5", "Feature 5", Feature5, Feature5Source),
+  Feature6: entry("Feature6", "Feature 6", Feature6, Feature6Source),
+  Feature7: entry("Feature7", "Feature 7", Feature7, Feature7Source),
+  Feature8: entry("Feature8", "Feature 8", Feature8, Feature8Source),
+  Feature9: entry("Feature9", "Feature 9", Feature9, Feature9Source),
+  Feature10: entry("Feature10", "Feature 10", Feature10, Feature10Source),
+  Feature11: entry("Feature11", "Feature 11", Feature11, Feature11Source),
+  Feature12: entry("Feature12", "Feature 12", Feature12, Feature12Source),
+  Feature13: entry("Feature13", "Feature 13", Feature13, Feature13Source),
+  Marquee1: entry("Marquee1", "Marquee 1", Marquee1, Marquee1Source),
+  Marquee2: entry("Marquee2", "Marquee 2", Marquee2, Marquee2Source),
+  Marquee3: entry("Marquee3", "Marquee 3", Marquee3, Marquee3Source),
+  Marquee4: entry("Marquee4", "Marquee 4", Marquee4, Marquee4Source),
+  Marquee5: entry("Marquee5", "Marquee 5", Marquee5, Marquee5Source),
+  Process1: entry("Process1", "Process 1", Process1, Process1Source),
+  ProtectWhatYours1: entry("ProtectWhatYours1", "Protect What Yours 1", ProtectWhatYours1, ProtectWhatYours1Source),
+  ProtectWhatYours2: entry("ProtectWhatYours2", "Protect What Yours 2", ProtectWhatYours2, ProtectWhatYours2Source),
+  ProtectWhatyours3: entry("ProtectWhatyours3", "Protect What Yours 3", ProtectWhatyours3, ProtectWhatyours3Source),
+  ProtectWhatYours4: entry("ProtectWhatYours4", "Protect What Yours 4", ProtectWhatYours4, ProtectWhatYours4Source),
+  ProtectWhatYours5: entry("ProtectWhatYours5", "Protect What Yours 5", ProtectWhatYours5, ProtectWhatYours5Source),
+  Tab1: entry("Tab1", "Tab 1", Tab1, Tab1Source),
+  Tab2: entry("Tab2", "Tab 2", Tab2, Tab2Source),
+  WhyFileWithUs: entry("WhyFileWithUs", "Why File With Us", WhyFileWithUs, WhyFileWithUsSource),
+  ProtectYourWork: entry("ProtectYourWork", "Protect Your Work", ProtectYourWork, ProtectYourWorkSource),
+  WhatYouCanRegister: entry("WhatYouCanRegister", "What You Can Register", WhatYouCanRegister, WhatYouCanRegisterSource),
+  BrandRegistration: entry("BrandRegistration", "Brand Registration", BrandRegistration, BrandRegistrationSource),
+  WhyWorkWithUsSection: entry("WhyWorkWithUsSection", "Why Work With Us", WhyWorkWithUsSection, WhyWorkWithUsSectionSource),
+  WhyRegisterCopyRight: entry("WhyRegisterCopyRight", "Why Register Copyright", WhyRegisterCopyRight, WhyRegisterCopyRightSource),
+
   TextImage1: entry("TextImage1", "Text Image 1", TextImage1, TextImage1Source),
   TextImage2: entry("TextImage2", "Text Image 2", TextImage2, TextImage2Source),
   TextImage3: entry("TextImage3", "Text Image 3", TextImage3, TextImage3Source),
