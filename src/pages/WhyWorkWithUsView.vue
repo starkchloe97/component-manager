f44d16fc0a81513eda4bbe9bff3d411ed35da056
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 02:00:22 +0500

    Add editable Why Work With Us page

---
+++ src/pages/WhyWorkWithUsView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
+</script>
+
+<template>
+  <ComponentLibraryPage
+    title="Why Work With Us Components"
+    prefix="WhyWorkWithUs"
+    description="Browse the Why Work With Us component and open the shared editor to customize any element inside it."
+  />
+</template>
