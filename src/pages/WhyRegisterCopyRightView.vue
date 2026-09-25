c00ee79c7c26123e8e2a78c82f90dc1f33cfffb9
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 02:00:42 +0500

    Add editable Why Register Copyright page

---
+++ src/pages/WhyRegisterCopyRightView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
+</script>
+
+<template>
+  <ComponentLibraryPage
+    title="Why Register Copyright Components"
+    prefix="WhyRegisterCopyRight"
+    description="Browse the Why Register Copyright component and open the shared editor to customize any element inside it."
+  />
+</template>
