3b600448318d44f15adadfff406062c46b8642c6
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 01:59:45 +0500

    Use shared editable library for Feature Components

--- src/pages/FeatureSection.vue
+++ src/pages/FeatureSection.vue
@@ -1,35 +1,11 @@
 <script setup>
-
-    import Feature from '@/components/FeaturesSection.vue/Feature1.vue';
-    import Feature2 from '@/components/FeaturesSection.vue/Feature2.vue';
-    import Feature3 from '@/components/FeaturesSection.vue/Feature3.vue';
-    import Feature4 from '@/components/FeaturesSection.vue/Feature4.vue';
-    import Feature5 from '@/components/FeaturesSection.vue/Feature5.vue';
-    import Feature6 from '@/components/FeaturesSection.vue/Feature6.vue';
-    import Feature7 from '@/components/FeaturesSection.vue/Feature7.vue';
-    import Feature8 from '@/components/FeaturesSection.vue/Feature8.vue';
-    import Feature9 from '@/components/FeaturesSection.vue/Feature9.vue';
-    import Feature10 from '@/components/FeaturesSection.vue/Feature10.vue';
-    import Feature11 from '@/components/FeaturesSection.vue/Feature11.vue';
-    import Feature12 from '@/components/FeaturesSection.vue/Feature12.vue';
-    import Feature13 from '@/components/FeaturesSection.vue/Feature13.vue';
-
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
 <template>
-
-    <Feature />
-    <Feature2 />
-    <Feature3 />
-    <Feature4 />
-    <Feature5 />
-    <Feature6 />
-    <Feature7 />
-    <Feature8 />
-    <Feature9 />
-    <Feature10 />
-    <Feature11 />
-    <Feature12 />
-    <Feature13 />
-
+  <ComponentLibraryPage
+    title="Feature Components"
+    prefix="Feature"
+    description="Browse every Feature component and open the shared editor to customize any element inside it."
+  />
 </template>
