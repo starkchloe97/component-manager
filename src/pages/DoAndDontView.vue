2014c2b6c979ad49a5839a3d131456991a08890d
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 01:59:43 +0500

    Use shared editable library for Do & Don't Components

--- src/pages/DoAndDontView.vue
+++ src/pages/DoAndDontView.vue
@@ -1,17 +1,11 @@
 <script setup>
-
-import DoAndDont1 from '@/components/DoAndDontSection/DoAndDont1.vue';
-import DoAndDont2 from '@/components/DoAndDontSection/DoAndDont2.vue';
-import DoAndDont3 from '@/components/DoAndDontSection/DoAndDont3.vue';
-import DoAndDont4 from '@/components/DoAndDontSection/DoAndDont4.vue';
-
+import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 </script>
 
 <template>
-
-<DoAndDont1 />
-<DoAndDont2 />
-<DoAndDont3 />
-<DoAndDont4 />
-
+  <ComponentLibraryPage
+    title="Do & Don't Components"
+    prefix="DoAndDont"
+    description="Browse every Do & Don't component and open the shared editor to customize any element inside it."
+  />
 </template>
