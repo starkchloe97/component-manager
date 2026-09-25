3131535d7898c92d843ccd2e5d6b62d417f0c569
starkchloe97 <starkchloe97@gmail.com>
2026-09-26 02:08:28 +0500

    Show all Box components in library

--- src/pages/BoxesView.vue
+++ src/pages/BoxesView.vue
@@ -5,7 +5,7 @@ import ComponentLibraryPage from "@/components/editor/ComponentLibraryPage.vue";
 <template>
   <ComponentLibraryPage
     title="Boxes Components"
-    prefix="Boxes"
+    prefix="Box"
     description="Browse every Boxes component and open the shared editor to customize any element inside it."
   />
 </template>
